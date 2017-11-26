34844e71c2f24f6a2b3e64ee87102f296d1fa491
Bryan Le <bryan@bryanle.io>
2017-11-25 22:30:08 -0500

    Completed chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -7,7 +7,18 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
-
-function chunk(array, size) {}
+function chunk(array, size) {
+    var chunkArray = [];
+    var chunk = [];
+    
+    for (var i = 0; i < array.length; i++) {
+      chunk.push(array[i])
+      if (chunk.length === size || i === array.length - 1) {
+        chunkArray.push(chunk)
+        chunk = [];
+      }  
+    }
+    return chunkArray
+  }
 
 module.exports = chunk;
